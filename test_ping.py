123d914324a1342d57b362bfdb8e1cce3351eaf5
Mokhtar-dev <generalmk122@gmail.com>
2026-06-20 12:39:58 -0700

    I understood how .split() works

--- test_ping.py
+++ test_ping.py
@@ -1,14 +1,27 @@
 import subprocess
 
-
 print("Sending a background ping probe...")
-
-# Running the system command silently
-# We pass the command as a list of individual string arguments
 response = subprocess.run(["ping", "-n", "1", "8.8.8.8"], capture_output=True, text=True)
-
-# The text output is safely stored inside the 'stdout' attribute of our response object
 raw_output = response.stdout
 
-print("--- Capture Successful! Here is the text Python trapped: ---")
-print(raw_output)
+# Loop through the output line by line to find our target
+for line in raw_output.splitlines():
+    if "time=" in line:
+
+        part1 = line.split("time=")[1]
+        part2 = part1.split("ms")[0]
+        print(part1)
+        print(part2)
+
+
+
+
+        # --- YOUR FISHING CHALLENGE AREA ---
+        # 1. Split the line at "time=" and grab the right side (index 1)
+        # 2. Split that result at "ms" and grab the left side (index 0)
+        # 3. Type cast the final string into an integer
+
+        # Write your code here to isolate the number!
+        # ping_time = ...
+
+        # print(f"Successfully extracted integer: {ping_time}")
